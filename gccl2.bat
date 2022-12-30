b41f464ba02d8cff89f037ed686d39c4925faee4
qq290404 <2904045609@qq.com>
2022-12-31 00:47:12 +0800

    [环境更新]移除依赖环境，需要手动添加

--- gccl2.bat
+++ gccl2.bat
@@ -3,6 +3,8 @@
 set HOME=%USERPROFILE%
 set RootPath=%cd%
 
+call env.bat
+
 if exist tools\tmp GOTO OSBITNESSCHECK
 @mkdir tools\tmp 
 
@@ -22,5 +24,5 @@ echo 32-bit system detected
 GOTO END
 
 :END
-set path=%RootPath%\tools\linux\bin;%RootPath%\tools\toolchain\gcc-arm-none-eabi\bin;%RootPath%\tools\Python 2.7
+set path=%Project_Dependence_Path%
 C:\WINDOWS\system32\cmd.exe
